a8eedf24dc82b4416bd759111dd33b9a73b6b477
Risad212 <hafez.risad@gmail.com>
2022-06-08 19:06:22 +0600

    done

--- src/Components/CheckOutForm/CheckOutForm.js
+++ src/Components/CheckOutForm/CheckOutForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import './CheckOutForm.css';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import AmexCardImage from '../../Media/credit-cards_amex.png';
 import MasterCardImage from '../../Media/credit-cards_mastercard.png';
 import VisaCardImage from '../../Media/credit-cards_visa.png';
+import PayPalImage from '../../Media/Bitmap.png';
 
 const CheckOutForm = () => {
     const stripe = useStripe();
@@ -23,31 +24,51 @@ const CheckOutForm = () => {
         });
     };
     return (
-        <div className='border p-5'>
-            <form onSubmit={handleSubmit}>
-                <Row>
-                    <Col lg="8">
-                        <div className='creaditCard d-flex'>
-                            <input type="radio" />
-                            <div>
-                                <h3>Credit Card</h3>
-                                <p>Safe money transfer using your bank account. Visa, Maestro, Discover, American Express.</p>
+        <form onSubmit={handleSubmit}>
+            <Container>
+                <div className='border p-5 my-5'>
+                    <Row>
+                        <Col lg="8">
+                            <div className='creaditCard d-flex'>
+                                <input type="radio" />
+                                <div>
+                                    <h3>Credit Card</h3>
+                                    <p>Safe money transfer using your bank account. Visa, Maestro, Discover, American Express.</p>
+                                </div>
                             </div>
+                        </Col>
+                        <Col lg="4">
+                            <img src={AmexCardImage} className="me-2" alt="" />
+                            <img src={MasterCardImage} className="me-2" alt="" />
+                            <img src={VisaCardImage} className="me-2" alt="" />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <div className='CardNumber py-3'>
+                            <Col lg="12">
+                                <CardElement />
+                            </Col>
                         </div>
-                    </Col>
-                    <Col lg="4">
-                        <img src={AmexCardImage} className="me-2" alt="" />
-                        <img src={MasterCardImage} className="me-2" alt="" />
-                        <img src={VisaCardImage} className="me-2" alt="" />
-                    </Col>
-                </Row>
-                <Row>
-                    <div>
-                        <CardElement />
-                    </div>
-                </Row>
-            </form>
-        </div>
+                    </Row>
+                </div>
+                <div className='border p-5'>
+                    <Row>
+                        <Col lg="8">
+                            <div className='creaditCard d-flex'>
+                                <input type="radio" />
+                                <div>
+                                    <h3>PayPal</h3>
+                                    <p>Safe money transfer using your bank account. Visa, Maestro, Discover, American Express.</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col lg="4">
+                            <img src={PayPalImage} className="me-2" alt="" />
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+        </form>
     );
 };
 
